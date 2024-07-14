d285689a28d1da45777849bdcb1779ec21ff4f4e
Cyriac Launay <claunay@intersystems.com>
2024-07-14 22:43:24 +0200

    Initial commit for final project

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,17 +2,56 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 
 class ProductController extends AbstractController
 {
-    #[Route('/product', name: 'app_product')]
-    public function index(): Response
+    #[Route('/product', name: 'get_product', methods: 'GET')]
+    public function getProducts(ProductRepository $productRepository): JsonResponse
     {
-        return $this->render('product/index.html.twig', [
-            'controller_name' => 'ProductController',
-        ]);
+        $products = $productRepository->findAll();
+        return $this->json($products);
+    }
+
+    #[Route('/product', name: 'create_product', methods: 'POST')]
+    public function createProduct(Request $request, EntityManagerInterface $em): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+        $product = new Product();
+        $product->setName($data['name']);
+        $product->setPrice($data['price']);
+        $em->persist($product);
+        $em->flush();
+        return $this->json($product);
+    }
+
+    #[Route('/product/update', name: 'update_product', methods: ['POST'])]
+    public function updateProduct(Request $request, EntityManagerInterface $em, ProductRepository $productRepository): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+        $product = $productRepository->find($data['id']);
+        $product->setName($data['name']);
+        $product->setPrice($data['price']);
+        $em->flush();
+        return $this->json($product);
+    }
+
+    #[Route('/product/delete', name: 'delete_product', methods: 'POST')]
+    public function deleteProduct(Request $request, EntityManagerInterface $em, ProductRepository $productRepository): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+        $product = $productRepository->find($data['id']);
+        $productName = $product->getName();
+        $productPrice = $product->getPrice();
+        $em->remove($product);
+        $em->flush();
+
+        return $this->json("Le produit $productName avec un prix de $productPrice a été supprimé.");
     }
 }
